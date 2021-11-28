e2a3ba0c6510a608cf1b2f0a29645b89e3c2c8c0
lakshmi10113 <lakshmipriyasatheesh123@gmail.com>
2021-11-28 13:51:15 +0530

    first run

--- src/App.js
+++ src/App.js
@@ -1,74 +1,271 @@
 import './App.css';
 import React from 'react';
 // import { Button } from 'react-bootstrap';
-import Card from './Card.js';
+// import Card from './Card.js';
 import {useState} from 'react';
+// import {React ,useState} from 'react';
+
 
 function App() {
-  const detail=[
-  {
-    title:"Free",
-    price: 0,
-    user: "Single User",
-    storage: "5GB Storage",
-    public_project: "Unlimited Public Projects",
-    community_access:"Community Access",
-    private_project:"Unlimited Private Projects",
-    private_project_enable: false,
-    phone_support:"Dedicated Phone Support",
-    phone_support_enable: false,
-    subdomain:"Free Subdomain",
-    subdomain_enable:false,
-    status_report: "Monthly Status Reports",
-    status_report_enable: false
-  },
-  {
-    title:"Plus",
-    price: 10,
-    user: "5 Users",
-    storage: "50GB Storage",
-    public_project: "Unlimited Public Projects",
-    community_access:"Community Access",
-    private_project:"Unlimited Private Projects",
-    private_project_enable: true,
-    phone_support:"Dedicated Phone Support",
-    phone_support_enable: true,
-    subdomain:"Free Subdomain",
-    subdomain_enable:true,
-    status_report: "Monthly Status Reports",
-    status_report_enable: false
-  },
-  {
-    title:"Pro",
-    price: 29,
-    user: "Unlimited Users",
-    storage: "150GB Storage",
-    public_project: "Unlimited Public Projects",
-    community_access:"Community Access",
-    private_project:"Unlimited Private Projects",
-    private_project_enable: true,
-    phone_support:"Dedicated Phone Support",
-    phone_support_enable: true,
-    subdomain:"Free Subdomain",
-    subdomain_enable:true,
-    status_report: "Monthly Status Reports",
-    status_report_enable: true,
-    unlimited:"Unlimited"
-  }
-];
+ let [cartItems,setCartItems] = useState([]);
+ let [cart,setCart] = useState(0);
+//  let [select,setSelect] = useState(0);
+ let [total,setTotal] = useState(0);
+
+
+
+ function addCart(item){
+     setCart(cart +1);
+    //  setSelect(prev => !prev);
+     setTotal(prev => prev+item.price);
+     cartItems.push(item);
+     console.log(item.name);
+ }
+
+ function removeCart(value){
+    setCart(prev => prev - 1);
+    // setSelect(prev => !prev);
+    setTotal(prev => prev-value);
+ }
+
+ function deleteItem(value){
+    cartItems.splice(cartItems.indexOf(value),1);
+    setCart(cart - 1);
+    // setSelect(prev => !prev);
+    setTotal(prev => prev-value.price);
+
+ }
   return (
     <div>
    
-   <section class="pricing py-5">
-  <div class="container">
-    <div class="row">
-      <Card type={detail[0]}/>
-      <Card type={detail[1]}/>
-      <Card type={detail[2]}/>
-
-    </div>
-  </div>
-</section>
+   
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#!">Start Bootstrap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Shop</a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#!">All Products</a></li>
+                                <li><hr class="dropdown-divider" /></li>
+                                <li><a class="dropdown-item" href="#!">Popular Items</a></li>
+                                <li><a class="dropdown-item" href="#!">New Arrivals</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="submit">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill">{cart}</span>
+                        </button>
+                    </form>
+                    <div class="ml-3">
+                        Price:${total}
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <section>
+            <div style={{paddingLeft: "110px",paddingTop:"20px"}} >
+            <strong>Cart Items:</strong> {cartItems.map(e=> {
+                return <div>{e.name} <em>${e.price}</em> <button class="my-2" onClick={() => deleteItem(e)}>Delete Item</button></div>
+            })}
+            </div>
+        </section>
+        {/* <header class="bg-dark py-5">
+            <div class="container px-4 px-lg-5 my-5">
+                <div class="text-center text-white">
+                    <h1 class="display-4 fw-bolder">Shop in style</h1>
+                    <p class="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+                </div>
+            </div>
+        </header> */}
+        <section class="py-5">
+            <div class="container px-4 px-lg-5 mt-5">
+                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    $40.00 - $80.00
+                                </div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <div class="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div>
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Special Item</h5>
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <span class="text-muted text-decoration-line-through">$20.00</span>
+                                    $18.00
+                                </div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center">
+                                    {
+                                        false ? <button class="btn btn-outline-dark mt-auto" onClick={() => removeCart(18)}>Remove from cart</button> :
+                                        <button class="btn btn-outline-dark mt-auto" onClick={() => addCart({name:"Special Item",price:18})}>Add to cart</button>
+                                        
+                                    }
+                                    {/* {
+                                        select ?   <button disabled class="btn btn-outline-dark mt-auto" onClick={() => addCart({name:"special-item",price:18})}>Add to cart</button> :
+                                        <button class="btn btn-outline-dark mt-auto" onClick={() => addCart({name:"special-item",price:18})}>Add to cart</button>
+                                        
+                                    } */}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <div class="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div>
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Sale Item</h5>
+                                    <span class="text-muted text-decoration-line-through">$50.00</span>
+                                    $25.00
+                                </div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center">
+                                    {
+                                        false ?  <button class="btn btn-outline-dark mt-auto" onClick={()=>removeCart(25)}>Remove from cart</button>
+                                        :
+                                        <button class="btn btn-outline-dark mt-auto" onClick={() => addCart({name:"Sale Item",price:25})}>Add to cart</button>
+                                    }
+                                     {/* {
+                                        select ?   <button disabled class="btn btn-outline-dark mt-auto" onClick={() => addCart({name:"sale-item",price:25})}>Add to cart</button> :
+                                        <button class="btn btn-outline-dark mt-auto" onClick={() => addCart({name:"sale-item",price:25})}>Add to cart</button>
+                                        
+                                    } */}
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Popular Item</h5>
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    $40.00
+                                </div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><button class="btn btn-outline-dark mt-auto" onClick={() => addCart({name:"Popular Item",price:40})}>Add to cart</button></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <div class="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div>
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Sale Item</h5>
+                                    <span class="text-muted text-decoration-line-through">$50.00</span>
+                                    $25.00
+                                </div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Fancy Product</h5>
+                                    $120.00 - $280.00
+                                </div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View options</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <div class="badge bg-dark text-white position-absolute" style={{top: "0.5rem", right: "0.5rem"}}>Sale</div>
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Special Item</h5>
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    <span class="text-muted text-decoration-line-through">$20.00</span>
+                                    $18.00
+                                </div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <h5 class="fw-bolder">Popular Item</h5>
+                                    <div class="d-flex justify-content-center small text-warning mb-2">
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                        <div class="bi-star-fill"></div>
+                                    </div>
+                                    $40.00
+                                </div>
+                            </div>
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
 
 
 
